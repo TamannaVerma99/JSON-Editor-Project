98c3e9f4c4e3dacc64f5ffce61ec7f56087962ba
Tamanna <96949249+TamannaVerma99@users.noreply.github.com>
2024-03-17 21:44:57 +0530

    Update index.tsx

--- pages/editor1/index.tsx
+++ pages/editor1/index.tsx
@@ -5,10 +5,10 @@ import Hello from '~/components/Hello';
 const App1: React.FC = () => { 
   return (
     <>
-    <div className='flex h-[100vh] w-full flex-col bg-gradient-to-r from-startBlue from-1.95% to-endBlue'>
+    <div className='flex h-[100vh] w-full mt-[85px] lg:mt-[80px] md:mt-[85px] flex-col bg-gradient-to-r from-startBlue from-1.95% to-endBlue'>
     <Hello schemaType="schema"/>
     <Link href="/secondstep">
-      <div style={{ margin:'2%',display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div className='m-2 flex flex-col items-center'>
         <button className=' w-full md:w-1/2 px-[10px] py-[20px] bg-primary text-white rounded border-2 text-white '>
          Move to next step
         </button>
