297a37a96ba91be3a30d0b4f1d528af4789527f2
Tamanna <96949249+TamannaVerma99@users.noreply.github.com>
2024-03-23 15:49:24 +0530

    Update index.tsx

--- pages/editor2/index.tsx
+++ pages/editor2/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useCallback } from 'react';
 import Link from 'next/link';
-import Hello from '~/components/Hello';
+import Hello from '~/components/codeeditor';
 const App2: React.FC = () => { 
   return (
     <>
