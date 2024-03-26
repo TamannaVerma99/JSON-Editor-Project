84b3dd451e86ed49dca88a4a453d562eeb0b05b5
Tamanna <96949249+TamannaVerma99@users.noreply.github.com>
2024-03-26 10:47:39 +0530

    Update index.tsx

--- pages/firststep/index.tsx
+++ pages/firststep/index.tsx
@@ -1,33 +1,39 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from 'next/link';
-import hljs from "highlight.js/lib/core";
-import javascript from "highlight.js/lib/languages/javascript";
 import "highlight.js/styles/monokai.css";
-import { useState } from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 
-hljs.registerLanguage("javascript", javascript);
 
 export default function Home() {
   const [copied, setCopied] = useState(false);
-  const codeRef = useRef<HTMLElement | null>(null); // Specify the type of useRef
+  const codeRef = useRef<HTMLDivElement>(null); // Specify the type of useRef
  
-  useEffect(() => {
-    if (codeRef.current) { // Check if codeRef.current is not null
-      hljs.highlightBlock(codeRef.current);
-    }
-  }, []);
   const copyToClipboard = () => {
     if (codeRef.current) {
-      navigator.clipboard.writeText(codeRef.current.textContent || '');
-    }
-    setCopied(true);
+      const textArea = document.createElement('textarea');
+      textArea.value = codeRef.current.textContent || '';
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+      setCopied(true);
 
-    // Reset the button text after 2 seconds
-    setTimeout(() => {
-      setCopied(false);
-    }, 2000);
+      // Reset the button text after 2 seconds
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
   };
+  useEffect(() => {
+    if (codeRef.current) {
+      const preElement = codeRef.current.querySelector('pre');
+      if (preElement) {
+        preElement.style.textAlign = 'center';
+      }
+    }
+  }, []);
+
 
   return (
     <main className='mt-0 p-0 h-full w-full flex flex-col items-center bg-gradient-to-r from-startBlue from-1.95% to-endBlue bg-no-repeat'>
@@ -50,6 +56,23 @@ export default function Home() {
           <div className='max-w-[700px] text-center lg:mt-2 mb-6 mx-auto'>
             Imagine you have a magical potion that holds the power of information. This potion, known as JSON, is made up of key-value pairs, just like the secrets whispered between wizards. Here's a glimpse:
           </div>
+          <div className='max-w-[700px] lg:w-[700px] md:w-[500px] text-center mx-auto lg:mt-2 relative '>
+    <SyntaxHighlighter language="javascript" >{`
+                            {"wizard": "merlin",
+                              "spell": "Abracadabra",
+                              "power": "4" }
+      `}
+      </SyntaxHighlighter>
+  
+</div>
+<h1 className='lg:leading-header text-h1mobile lg:text-h3 font-semibold text-white text-center px-1 md:px-0 mx-auto'>
+            Let's learn about JSON Schema
+          </h1>
+          <div className='max-w-[700px] text-center lg:mt-2 mb-6 mx-auto'>
+          Now, imagine we want to ensure that any JSON data representing a wizard follows the same structure and has the same properties.That's where JSON Schema comes in.  
+          <p><b>JSON Schema is like a set of guidelines or rules that specify the structure and content of JSON data. It defines what properties are allowed, what data types they should have, and any additional constraints they must meet.</b></p>
+        
+          </div>
           <div className='max-w-[700px] lg:w-[700px] md:w-[500px] text-center mx-auto lg:mt-2 relative'>
   
   <button
@@ -58,19 +81,39 @@ export default function Home() {
   >
     {copied ? "Copied" : "Copy"}
   </button>
-  <pre>
-    <code className="javascript" style={{ background: "transparent",backgroundColor:'#2b2b2b',color:"#ffffff !important" ,padding: "20px",borderRadius:"10px"}} ref={codeRef}>
-      {`
-{ "wizard": "merlin",
-  "spell": "Abracadabra",
-  "power": "Invisibility" }
-      `}
-    </code>
-  </pre>
+  <div  ref={codeRef}>
+  <SyntaxHighlighter language="javascript" >
+    {`{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://example.com/wizard.schema.json",
+  "title": "Wizard",
+  "description": "Information about a wizard",
+  "type": "object",
+  "properties": {
+    "wizard": {
+      "type": "string"
+    },
+    "spell": {
+      "type": "string"
+    },
+    "power": {
+      "type": "number"
+    }
+  },
+  "required": ["wizard", "spell", "power"]
+}`}
+</SyntaxHighlighter>
 </div>
-
+</div>
+<h1 className='lg:leading-header text-h1mobile lg:text-h3 font-semibold text-white text-center px-1 md:px-0 mx-auto'>
+           $Schema Keyword
+          </h1>
+<div className='max-w-[700px] text-center lg:mt-2 mb-6 mx-auto'>
+The $schema keyword in JSON Schema is used to indicate the version of the JSON Schema standard being used. It provides a URL that points to the JSON Schema draft being followed for validating the JSON data.
+   Here,the $schema keyword is set to "https://json-schema.org/draft/2020-12/schema", which indicates that the JSON Schema is based on the draft version 2020-12 of the JSON Schema standard.  
+        </div>
           <div className='lg:w-[650px] mx-auto my-10 flex flex-col gap-6 items-center justify-center'>
-            Can you decipher the secrets hidden within? Explore the JSON realm and unlock the mysteries of structured data. Ready for an adventure? Click the button below to embark on a journey through the JSON Kingdom!
+          Ready to embark on an adventure into the JSON realm? Test your skills by crafting a valid JSON Schema for Draft 2020-12! Click the button below to begin your journey through the JSON Kingdom!
             <Link href='/editor1' className='flex items-center justify-center rounded border-2 border-white text-white w-[194px] h-[40px] font-semibold'>Try it yourself</Link>
           </div>
         </div>
